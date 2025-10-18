c8264bab10d8d334a49b3bb97c6b613497326bce
Rishabh Raturi <rishabhraturi05@gmail.com>
2025-10-18 11:48:11 +0530

    details-array

--- src/app/components/Membersarray.js
+++ src/app/components/Membersarray.js
@@ -158,180 +158,180 @@ export const allmembers = {
     software: [
         {
             name: `Rishabh Raturi`,
-            email: `rishabhraturi05@gmail.c`,
+            email: `rishabhraturi05@gmail.com`,
             img: `/members/software/Rishab Raturi.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/rishabh-raturi-18961626a/',
+            instagram: 'https://www.instagram.com/therishabhraturi/'
         },
         {
             name: `Rudransh Singh`,
-            email: `rs23meb0b10@student.r`,
+            email: `rs23meb0b10@student.nitw.ac.in`,
             img: `/members/software/Rudransh Singh.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/rudransh-singh-b2358b27a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/__rs_dominator_?igsh=MWUyZ2t5NGN3MzVqeA=='
         },
         {
             name: `Aritra Mandal`,
-            email: `am23meb0b14@student`,
+            email: `am23meb0b14@student.nitw.ac.in`,
             img: `/members/software/Aritra Mandal.jpg`,
-            linkedin: 'https://www.linkedin.com/',
+            linkedin: 'https://www.linkedin.com/in/eigenstein/',
             instagram: '#'
         },
         {
             name: `Arnav Jain`,
-            email: `ar24eeb0b22@student.n`,
+            email: `ar24eeb0b22@student.nitw.ac.in`,
             img: `/members/software/Arnav Ranjeetkumar Jain.jpeg`,
-            linkedin: 'www.linkedin.com/in/arna',
-            instagram: '#'
+            linkedin: 'www.linkedin.com/in/arnav-jain-4480a9305',
+            instagram: 'arnav'
         },
         {
             name: `Aryan Roy`,
             email: `aryan.roy.w@gmail.com`,
             img: `/members/addsecs/Aryan Roy.jpg`,
-            linkedin: 'https://www.linkedin.com/oopsie doopsie',
-            instagram: '#'
+            linkedin: 'https://www.linkedin.com/in/aryan-roy-8b0b89285?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'oopsie doopsie'
         },
         {
             name: `Srijan Singh`,
-            email: `ss23meb0a41@student.`,
+            email: `ss23meb0a41@student.nitw.ac.in`,
             img: `/members/software/Srijan Singh.jpg`,
             linkedin: '#',
             instagram: '#'
         },
         {
             name: `Vipul Bansal`,
-            email: `vipulbansal.2901@gmail`,
+            email: `vipulbansal.2901@gmail.com`,
             img: `/members/software/Vipul Bansal.png`,
-            linkedin: 'linkedin.com/in/vipul-bans..',
+            linkedin: 'linkedin.com/in/vipul-bansal-7a926238a',
             instagram: '#'
         },
         {
             name: `Dasari Rahul`,
-            email: `dr24csb1a12@student.n`,
+            email: `dr24csb1a12@student.nitw.ac.in`,
             img: `/members/software/Dasari Rahul.png`,
-            linkedin: 'www.linkedin.com/in/dasa',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'http://www.linkedin.com/in/dasari-rahul-1440b1363',
+            instagram: 'https://www.instagram.com/rahul2.1_7'
         },
         {
             name: `Rijul Kathpal`,
             email: `rijulkathpal@gmail.com`,
             img: `/members/software/Rijul Kathpal.png`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/rijul-kathpal',
+            instagram: 'https://www.instagram.com/rijul.kathpal'
         },
         {
             name: `Naman Kumar`,
-            email: `nk24csb0a48@student.n`,
+            email: `nk24csb0a48@student.nitw.ac.in`,
             img: `/members/software/Naman Kumar.jpeg`,
-            linkedin: 'www.linkedin.com/in/nam',
+            linkedin: 'http://www.linkedin.com/in/naman-kumar-5634321aa',
             instagram: '#'
         },
         {
             name: `Nikhitha Kandasamy`,
-            email: `nk24csb0b50@student.n`,
+            email: `nk24csb0b50@student.nitw.ac.in`,
             img: `/members/software/Nikhitha Kandasamy.png`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/nikhitha-kandasamy-9a124b349/',
+            instagram: 'https://www.instagram.com/__.nikkis._/'
         },
         {
             name: `Sudhanshu Bhagat`,
-            email: `sb24ecb0b57@student.r`,
+            email: `sb24ecb0b57@student.nitw.ac.in`,
             img: `/members/software/Sudhanshu Bhagat.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/sudhanshu-bhagat-a33538313?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/sudhanshubhagat4?igsh=NXNseW5sdGh3YXNs'
         },
         {
             name: `Borra Pardhiv`,
-            email: `bp24csb1a10@student.r`,
+            email: `bp24csb1a10@student.nitw.ac.in`,
             img: `/members/software/Borra Pardhiv.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/borra-pardhiv-504198340?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/pardhiv_b007?igsh=MTR4aHJtYnhjY244dw=='
         },
         {
             name: `Anupama Chinta`,
-            email: `ca24mae0004@student.`,
+            email: `ca24mae0004@student.nitw.ac.in`,
             img: `/members/software/Chinta Anupama.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/anupama-chinta-8347b532b',
+            instagram: 'https://www.instagram.com/ch_anupama_18?igsh=eWxzYTQ3b3J1NTJm'
         }
     ],
     mechanical: [
         {
             name: `Wafiyyah EK`,
-            email: `we23meb0b49@student`,
+            email: `we23meb0b49@student.nitw.ac.in`,
             img: `/members/mech/Wafiyyah Ek.png`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/wafiyyah-ek',
+            instagram: 'https://www.instagram.com/wafiyyah_ek/'
         },
         {
             name: `Dinesh Pichaappan`,
-            email: `dinesh21106@gmail.con`,
+            email: `dinesh21106@gmail.com`,
             img: `/members/mech/Dinesh Pichaappan.jpeg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/dinesh-pichaappan-a29484355?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BXSzFTxSwRRaMLATWyuYG3g%3D%3D',
+            instagram: 'https://www.instagram.com/dineshpichaappan?igsh=MWxodWI3YmhnZXZkNA%3D%3D&utm_source=qr'
         },
         {
             name: `Palaka Vishnu`,
-            email: `pv24meb0a45@student.`,
+            email: `pv24meb0a45@student.nitw.ac.in`,
             img: `/members/mech/Palaka Vishnu.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/vishnu-palaka-b58610355',
+            instagram: 'https://www.instagram.com/vishnu___42407?igsh=MW5uNmk4MDl5MjJydA=='
         },
         {
             name: `Sabbini Sai Datta`,
-            email: `ss24meb0a55@student.`,
+            email: `ss24meb0a55@student.nitw.ac.in`,
             img: `/members/mech/Sabbini Sai Datta.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/sai-datta-sabbini-14b525311',
+            instagram: 'https://www.instagram.com/saidatta.203?igsh=N3Rpbm43ODN5eHZv'
         },
         {
             name: `Soumilya Dwivedi`,
             email: `soumilya.d@gmail.com`,
             img: `/members/mech/Soumilya Dwivedi.jpeg`,
-            linkedin: 'https://www.linkedin.com/',
+            linkedin: 'https://www.linkedin.com/in/soumilya-dwivedi-93aa60321/',
             instagram: '#'
         },
         {
             name: `Sai Vishnu`,
             email: `tsaivishnuteja@gmail.com`,
             img: `/members/mech/Tumulur Sai Vishnu Teja.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/sai-vishnu-teja-tumulur-15285528b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/viz_0405?igsh=NmVlY3Vnc3p1anl2'
         },
         {
             name: `Sai Vikas Neela`,
-            email: `saivikasneela1808@gma`,
+            email: `saivikasneela1808@gmail.com`,
             img: `/members/mech/Neela Saivikas.jpg`,
-            linkedin: 'https://www.linkedin.com/',
+            linkedin: 'https://www.linkedin.com/in/sai-vikas-neela ',
             instagram: '#'
         },
         {
             name: `Raghuveer pusunuri`,
-            email: `Rp23meb0a35@student.`,
+            email: `Rp23meb0a35@student.nitw.ac.in`,
             img: `/members/mech/Raghuveer Pusunuri.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/raghuveer-pusunuri-0a5855328?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/raghuveer_pr?igsh=MTBnbDFxNnI3MWxkNA=='
         },
         {
             name: `Roy Harwani`,
-            email: `rh24mab0a29@student.`,
+            email: `rh24mab0a29@student.nitw.ac.in`,
             img: `/members/mech/Roy - Roy Harwani.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/roy-harwani-5030a6312/',
+            instagram: 'https://www.instagram.com/harwani.roy/?next=%2F'
         },
         {
             name: `ARKA ROY CHOUDHURY`,
-            email: `ar24meb0a10@student.r`,
+            email: `ar24meb0a10@student.nitw.ac.in`,
             img: `/members/mech/Arka Roy Choudhury.jpg`,
-            linkedin: 'www.linkedin.com/in/arka',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'www.linkedin.com/in/arka-roy-choudhury-b043902a7',
+            instagram: 'https://www.instagram.com/arka.77101/'
         },
         {
             name: `Sudhakar Kumar`,
-            email: `krsudhakar1103@gmail.`,
+            email: `krsudhakar1103@gmail.com`,
             img: `/members/gensecs/_Sudhakar_photo - Sudhakar Kumar.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/sudhakar-kumar-38a9a4282/',
+            instagram: 'https://www.instagram.com/_sudhakar.08?igsh=MWcxeTJrb2J5OWI2bA=='
         },
         {
             name: `M. Mani Kumar`,
@@ -393,45 +393,45 @@ export const allmembers = {
     pr: [
         {
             name: `Gaurav Rastogi`,
-            email: `gauravrastogi2911@gma`,
+            email: `gauravrastogi2911@gmail.com`,
             img: `/members/PR/Gaurav Rastogi.jpg`,
-            linkedin: 'https://www.linkedin.com/',
+            linkedin: 'https://www.linkedin.com/in/gaurav-rastogi-nitw?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
             instagram: '#'
         },
         {
             name: `Deva kumar Peta`,
-            email: `devakumar0167@gmail.`,
+            email: `devakumar0167@gmail.com`,
             img: `/members/PR/Deva kumar Peta.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_apphttps://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_apphttps://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_apphttps://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_apphttps://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_apphttps://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_apphttps://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_apphttps://www.linkedin.com/in/deva-kumar-peta-440b2a282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/the_petabytee?igsh=OWJ3b3FuaHAxZzJl'
         },
         {
             name: `Sachin kumar`,
             email: `sk4875330@gmail.com`,
             img: `/members/PR/Sachin Kumar.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: '#'
+            linkedin: 'https://www.linkedin.com/in/%F0%9D%97%A6%F0%9D%97%AE%F0%9D%97%B0%F0%9D%97%B5%F0%9D%97%B6%F0%9D%97%BB-%F0%9D%97%B8%F0%9D%98%82%F0%9D%97%BA%F0%9D%97%AE%F0%9D%97%BF-629813290?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'sk_9.45'
         },
         {
             name: `Muhammad Nurul`,
-            email: `mn24edi0009@student.r`,
+            email: `mn24edi0009@student.nitw.ac.in`,
             img: `/members/PR/Muhammad Nurul.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/muhammad-nurul-hayi-b13184330?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/uniquenoorulhayi?igsh=cWt2cGF0dzJvbXRt    '
         },
         {
             name: `Arif`,
-            email: `sa24mab0a34@student.`,
+            email: `sa24mab0a34@student.nitw.ac.in`,
             img: `/members/PR/Shaik Arif.jpg`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/arif-shaik-994837318',
+            instagram: 'https://www.instagram.com/_arif___shaik_'
         },
         {
             name: `Aryan`,
-            email: `chopraaryan846@gmail.`,
+            email: `chopraaryan846@gmail.com`,
             img: `/members/PR/Aryan Nit w.png`,
-            linkedin: 'https://www.linkedin.com/',
-            instagram: 'https://www.instagram.com'
+            linkedin: 'https://www.linkedin.com/in/aryannitw?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            instagram: 'https://www.instagram.com/chopra_00013?igsh=Y202MW15NXVtaGFq'
         }
     ],
 }
